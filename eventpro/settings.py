a67dbd4ef3cc49c44f456704126a4c0011021a39
nathaniel nosa <nathanielnarth@gmail.com>
2025-01-16 15:46:41 +0100

    event

--- eventpro/settings.py
+++ eventpro/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&lq_7h(yr86+0heuaf06#mz%gs43(yjd7i)1l(ty^-2(ftaaux'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://eventsolomon.onrender.com','localhost']
 
 
 # Application definition
